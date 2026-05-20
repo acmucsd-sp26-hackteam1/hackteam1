98467273f446da6624447c07ca5f6dc0c27c1b2b
Luke Wang <48535279+KingSkyrock@users.noreply.github.com>
2026-05-19 23:36:09 -0700

    Login with google skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import { Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import CalendarTest from './pages/CalendarTest.jsx'
+import Login from './pages/Login.jsx'
 import NotFound from './pages/NotFound.jsx'
 
 function App() {
   return (
     <div className="content">
       <nav className="nav">
+        <span className="nav-spacer" />
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/calendartest">Test Calendar</Link>
+        <Link to="/login" className="nav-login">Login</Link>
       </nav>
       <Routes className="body-content">
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/calendartest" element={<CalendarTest />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <div className="footer">
